532309f38a4d8a549c1fc8d4abf11ba82f9fd378
turtlekazu <turtlekazu@icloud.com>
2024-07-13 13:48:03 +0900

    feat(ui): add QuakeContent and remove default component

---
+++ core/src/commonMain/kotlin/com/ttllab/quake_compose/core/QuakeContent.kt
@@ -0,0 +1,39 @@
+package com.ttllab.quake_compose.core
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QuakeContent(
+    modifier: Modifier = Modifier,
+    rotationEnabled: Boolean = true,
+    positionEnabled: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .background(backgroundColor)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .rotate(if (rotationEnabled) rotationZ else 0f)
+                .offset(
+                    x = if (positionEnabled) offsetX.dp else 0.dp,
+                    y = if (positionEnabled) offsetY.dp else 0.dp
+                )
+        ) {
+            content()
+        }
+    }
+}
