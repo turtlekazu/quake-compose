589c181c794c94d685334b711266320214be36a4
turtlekazu <turtlekazu@icloud.com>
2024-07-13 13:53:37 +0900

    feat(ui): remove surrounding box with backgroundColor

--- core/src/commonMain/kotlin/com/ttllab/quake_compose/core/QuakeContent.kt
+++ core/src/commonMain/kotlin/com/ttllab/quake_compose/core/QuakeContent.kt
@@ -1,13 +1,9 @@
 package com.ttllab.quake_compose.core
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
@@ -23,19 +19,12 @@ fun QuakeContent(
 
     Box(
         modifier = modifier
-            .background(backgroundColor)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .rotate(if (rotationEnabled) sensorController.rotationValue.value.z else 0f)
+            .offset(
+                x = if (positionEnabled) sensorController.accelerationValue.value.x.dp else 0.dp,
+                y = if (positionEnabled) sensorController.accelerationValue.value.y.dp else 0.dp
+            )
     ) {
-        Box(
-            modifier = Modifier
-                .rotate(if (rotationEnabled) sensorController.rotationValue.value.z else 0f)
-                .offset(
-                    x = if (positionEnabled) sensorController.accelerationValue.value.x.dp else 0.dp,
-                    y = if (positionEnabled) sensorController.accelerationValue.value.y.dp else 0.dp
-                )
-        ) {
-            content()
-        }
+        content()
     }
 }
