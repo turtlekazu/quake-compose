87c9a7239a6470b67cfc28384c0eba2a0d03972f
turtlekazu <turtlekazu@icloud.com>
2024-07-21 20:37:42 +0900

    feat(ui): add disposableEffect function to release listener

--- core/src/commonMain/kotlin/com/ttllab/quake_compose/core/QuakeContent.kt
+++ core/src/commonMain/kotlin/com/ttllab/quake_compose/core/QuakeContent.kt
@@ -3,10 +3,14 @@ package com.ttllab.quake_compose.core
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.DisposableEffectScope
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import com.ttllab.quake_compose.core.entity.Vector3D
 
 @Composable
 fun QuakeContent(
@@ -16,6 +20,32 @@ fun QuakeContent(
     content: @Composable () -> Unit,
 ) {
     val sensorController = remember { SensorController() }
+
+    DisposableEffect(Unit) {
+        sensorController.start()
+
+        onDispose {
+            sensorController.stop()
+        }
+    }
+
+    QuakeContentBox(
+        content = content,
+        rotationEnabled = rotationEnabled,
+        positionEnabled = positionEnabled,
+        sensorController = sensorController,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun QuakeContentBox(
+    content: @Composable () -> Unit,
+    rotationEnabled: Boolean = true,
+    positionEnabled: Boolean = true,
+    sensorController: SensorController,
+    modifier: Modifier = Modifier,
+) {
     val accelerationValue by sensorController.accelerationValue
     val rotationValue by sensorController.rotationValue
 
