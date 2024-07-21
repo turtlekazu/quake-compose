0f307c83c230be7ade9718aaf6b78cc5ff96e7ba
turtlekazu <turtlekazu@icloud.com>
2024-07-21 20:10:49 +0900

    feat(app): add enable/disable button to demo app

--- demo/composeApp/src/commonMain/kotlin/App.kt
+++ demo/composeApp/src/commonMain/kotlin/App.kt
@@ -1,17 +1,21 @@
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.ttllab.quake_compose.core.QuakeContent
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import quake_compose.demo.composeapp.generated.resources.Res
 import quake_compose.demo.composeapp.generated.resources.compose_multiplatform
 
@@ -19,18 +23,26 @@ import quake_compose.demo.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        QuakeContent {
-            var showContent by remember { mutableStateOf(false) }
-            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                Button(onClick = { showContent = !showContent }) {
-                    Text("Click me!")
+        var enabled by remember { mutableStateOf(true) }
+        QuakeContent(
+            rotationEnabled = enabled,
+            positionEnabled = enabled,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
                 }
-                AnimatedVisibility(showContent) {
-                    val greeting = remember { Greeting().greet() }
-                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(painterResource(Res.drawable.compose_multiplatform), null)
-                        Text("Compose: $greeting")
-                    }
+                Button(onClick = { enabled = !enabled }) {
+                    Text(if (enabled) "Disable" else "Enable")
                 }
             }
         }
